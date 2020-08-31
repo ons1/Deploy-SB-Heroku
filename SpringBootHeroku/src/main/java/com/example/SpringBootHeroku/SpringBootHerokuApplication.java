6275194577e6944ddceca87c65604bc48e0df22f
o.khemiri <ons.khemiri@sofrecom.com>
2020-08-31 17:56:18 +0100

    Add a controller

--- SpringBootHeroku/src/main/java/com/example/SpringBootHeroku/SpringBootHerokuApplication.java
+++ SpringBootHeroku/src/main/java/com/example/SpringBootHeroku/SpringBootHerokuApplication.java
@@ -2,6 +2,8 @@ package com.example.SpringBootHeroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootHerokuApplication {
@@ -9,5 +11,17 @@ public class SpringBootHerokuApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHerokuApplication.class, args);
 	}
+
+
+
+@RestController
+public class HelloController {
+
+		@GetMapping("/")
+		String hello() {
+
+			return "Hello Ons : Test Heroku Deployment";
+		}}
+
 
 }
