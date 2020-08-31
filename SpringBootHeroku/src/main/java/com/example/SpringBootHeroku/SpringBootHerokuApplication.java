e31e478f24b89c754f301f4a1eb18a3150884cb2
o.khemiri <ons.khemiri@sofrecom.com>
2020-08-31 17:34:45 +0100

    Add SB Heroku class

---
+++ SpringBootHeroku/src/main/java/com/example/SpringBootHeroku/SpringBootHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHerokuApplication.class, args);
+	}
+	System.out.println("Hello Ons");
+}
---
+++ SpringBootHeroku/src/test/java/com/example/SpringBootHeroku/SpringBootHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
