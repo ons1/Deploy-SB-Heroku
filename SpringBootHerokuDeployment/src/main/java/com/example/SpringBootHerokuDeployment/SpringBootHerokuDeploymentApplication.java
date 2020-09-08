b85eecac0d1e83b208abe0ed76001caab9cd5ac9
o.khemiri <ons.khemiri@sofrecom.com>
2020-09-08 15:14:09 +0100

    add files

---
+++ SpringBootHerokuDeployment/src/main/java/com/example/SpringBootHerokuDeployment/SpringBootHerokuDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootHerokuDeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHerokuDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHerokuDeploymentApplication.class, args);
+	}
+
+}
